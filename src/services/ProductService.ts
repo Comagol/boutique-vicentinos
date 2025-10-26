a413a1c58a4e80f0994b4290b3110b4d9074e870
Comagol <comabellafrancisco@gmail.com>
2025-10-25 21:01:43 -0300

    Add foundational structure to ProductService with initial comments and service export for product-related functionalities.

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,2 +1,8 @@
+//importo los modelos y tipos
 import { ProductModel } from '../models/Products';
 import { Product, ProductCategory } from '../types/Product';
+
+//Servicios para Productos
+export const ProductService = {
+
+}
