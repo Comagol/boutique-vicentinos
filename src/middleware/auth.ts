62f0ccf6ffcbf663d6ee076c7fd18ee31b836db4
Comagol <comabellafrancisco@gmail.com>
2025-10-30 23:29:13 -0300

    Add authentication middleware to verify JWT tokens and handle authorization errors.

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,37 @@
 import { Request, Response, NextFunction } from 'express';
 import { AuthService } from '../services/AuthService';
 import { JwtPayload } from '../types/User';
+import { header } from 'express-validator';
 
 //extiendo el request para inclui user
 export interface AuthenticatedRequest extends Request {
   user?: JwtPayload;
+}
+
+//middleware de autenticacion
+export const authenticate = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction): void => {
+    try {
+      const authHeader = req.headers.authorization;
+
+      if(!authHeader) {
+        res.status(401).json({error: 'No token provided'});
+        return;
+      }
+
+      const token = authHeader.split(' ')[1];
+      if(!token) {
+        res.status(401).json({error: 'Invalid token'});
+        return;
+      }
+
+      const decoded = AuthService.verifyToken(token);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      res.status(401).json({error: 'Unauthorized'});
+      return;
+    }
 }
