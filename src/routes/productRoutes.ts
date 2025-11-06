bafdec84305645b7143f176b4d4d28311f35fd81
Comagol <comabellafrancisco@gmail.com>
2025-11-06 00:14:25 -0300

    Add public product routes: implement endpoints to retrieve all products and fetch a product by ID.

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -4,3 +4,9 @@ import { authenticate, requireAdmin } from '../middleware/auth';
 
 const router = Router();
 
+// ======== RUTAS PUBLICAS ==========
+//Get all products /api/products (PUBLIC)
+router.get('/', productController.getAllProducts);
+
+//Get product by id /api/products/:id (PUBLIC)
+router.get('/:id', productController.getProductById);
