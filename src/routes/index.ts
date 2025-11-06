7c68fa71320f43e0f33d1d1562cdb0eced846b4a
Comagol <comabellafrancisco@gmail.com>
2025-11-06 18:51:05 -0300

    Refactor main router: consolidate route management by integrating auth, product, order, and payment routes in index.ts, improving code organization and maintainability.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,3 +3,13 @@ import authRoutes from "./authRoutes";
 import productRoutes from "./productRoutes";
 import orderRoutes from "./orderRoutes";
 import paymentRoutes from "./paymentRoutes";
+
+const router = Router();
+
+//montar todas las rutas
+router.use('/auth', authRoutes);
+router.use('/products', productRoutes);
+router.use('/orders', orderRoutes);
+router.use('/payments', paymentRoutes);
+
+export default router;
