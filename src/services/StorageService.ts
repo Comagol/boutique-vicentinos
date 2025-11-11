61d8d196d46a50c0826751a22b8dd76fd7c540c5
Comagol <comabellafrancisco@gmail.com>
2025-11-10 22:51:56 -0300

    Add initial implementation of StorageService: set up Firebase Storage configuration and define the bucket for product images.

---
+++ src/services/StorageService.ts
@@ -0,0 +1,10 @@
+import { stroge } from '../config/firebase';
+import path from 'path';
+import fs from 'fs';
+
+//configuro el bucket de firebase Storage
+const BUCKET_NAME = 'products'; //dentro de esta carpeta se guardan las imagenes de los productos
+
+export const StorageService = {
+  
+}
