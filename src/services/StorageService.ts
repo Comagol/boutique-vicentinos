b5bbc767cbeef6581ba2b704c562e6ecfe7f72a4
Comagol <comabellafrancisco@gmail.com>
2025-11-10 22:56:36 -0300

    Implement file upload functionality in StorageService: add uploadFile method to handle file uploads to Firebase Storage, including metadata configuration and public access.

--- src/services/StorageService.ts
+++ src/services/StorageService.ts
@@ -1,10 +1,36 @@
-import { stroge } from '../config/firebase';
+import { storage } from '../config/firebase';
 import path from 'path';
 import fs from 'fs';
 
 //configuro el bucket de firebase Storage
 const BUCKET_NAME = 'products'; //dentro de esta carpeta se guardan las imagenes de los productos
 
+//servicio para manejar la subida de archivos a firebase Storage
 export const StorageService = {
-  
+ /**
+   * Sube un archivo a Firebase Storage y retorna su URL pública
+   * @param filePath - Ruta local del archivo temporal
+   * @param fileName - Nombre del archivo (se usará en Storage)
+   * @returns URL pública del archivo en Firebase Storage
+   */
+  async uploadFile(filePath: string, fileName: string): Promise<string> {
+    try {
+      const bucket = storage.bucket();
+
+      const storaPath = `${BUCKET_NAME}/${fileName}`;
+      const file = bucket.file(storagePath);
+
+      await bucket.upload(filePath,{
+        destination: storagePath,
+        metadata: {
+          contentType: this.getContentType(fileName),
+          cacheControl: 'public, max-age=31536000',
+        },
+      });
+
+      await file.makePublic();
+    } catch (error) {
+      
+    }
+  }
 }
